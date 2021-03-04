835a56464b3864c41a1be816ce4bc544766b4a63
Pablo Garrido <pablogs9@gmail.com>
2021-03-04 12:36:07 +0100

    Update

--- include/uxr/client/profile/multithread/multithread.h
+++ include/uxr/client/profile/multithread/multithread.h
@@ -12,18 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef UXR_CLIENT_PROFILE_MULTITHREAD_INTERNAL_H_
-#define UXR_CLIENT_PROFILE_MULTITHREAD_INTERNAL_H_
+#ifndef UXR_CLIENT_PROFILE_MULTITHREAD_H_
+#define UXR_CLIENT_PROFILE_MULTITHREAD_H_
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-
-#include <uxr/client/config.h>
-
-#include <uxr/client/visibility.h>
-#include <uxr/client/core/session/session.h>
 
 #include <uxr/client/config.h>
 
@@ -47,60 +42,8 @@ typedef struct uxrMutex{
 #endif    
 } uxrMutex;
 
-// #ifdef UCLIENT_PROFILE_MULTITHREAD
-// #define UXR_INIT_LOCK_SESSION uxr_init_session_lock(session)
-// #define UXR_LOCK_SESSION uxr_lock_session(session)
-// #define UXR_UNLOCK_SESSION uxr_unlock_session(session)
-// #else
-#define UXR_INIT_LOCK_SESSION uxr_init_lock(&session->mutex)
-#define UXR_LOCK_SESSION(session) uxr_lock(&session->mutex)
-#define UXR_UNLOCK_SESSION(session) uxr_unlock(&session->mutex)
-// #endif
-
-#define UXR_INIT_LOCK(X) uxr_init_lock(X)
-// #define UXR_LOCK(X) uxr_lock(X)
-// #define UXR_UNLOCK(X) uxr_unlock(X)
-
-#define UXR_LOCK_STREAM_ID(session, stream_id) uxr_lock(uxr_get_stream_mutex_from_id(session, stream_id))
-#define UXR_UNLOCK_STREAM_ID(session, stream_id) uxr_unlock(uxr_get_stream_mutex_from_id(session, stream_id))
-
-#define UXR_LOCK_ALL_INPUT_STREAMS(session) \
-    for(uint8_t i = 0; i < session->streams.input_best_effort_size; ++i){ uxr_lock(&session->streams.input_best_effort[i].mutex); } \
-    for(uint8_t i = 0; i < session->streams.input_reliable_size; ++i){ uxr_lock(&session->streams.input_reliable[i].mutex); } 
-
-#define UXR_UNLOCK_ALL_INPUT_STREAMS(session) \
-    for(uint8_t i = 0; i < session->streams.input_best_effort_size; ++i){ uxr_unlock(&session->streams.input_best_effort[i].mutex); } \
-    for(uint8_t i = 0; i < session->streams.input_reliable_size; ++i){ uxr_unlock(&session->streams.input_reliable[i].mutex); }   
-
-/**
- * @brief
- * TODO
- */
-UXRDLLAPI void uxr_init_lock(uxrMutex* mutex);
-
-/**
- * @brief
- * TODO
- */
-UXRDLLAPI void uxr_lock(uxrMutex* mutex);
-
-/**
- * @brief
- * TODO
- */
-UXRDLLAPI void uxr_unlock(uxrMutex* mutex);
-
-
-/**
- * @brief
- * TODO
- */
-UXRDLLAPI uxrMutex * uxr_get_stream_mutex_from_id(uxrSession* session, uxrStreamId stream_id);
-
-
-
 #ifdef __cplusplus
 }
 #endif
 
-#endif // UXR_CLIENT_PROFILE_MULTITHREAD_INTERNAL_H_
+#endif // UXR_CLIENT_PROFILE_MULTITHREAD_H_
